cf4472e5ea115248e1f0f675008415a61f671e8f
DavidBrev <brevers.david@gmail.com>
2020-07-25 23:55:19 +0200

    Add board generation

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 export default class Board extends React.Component{
+  constructor(props){
+    super(props);
+    let board = [];
+    for(let i=0; i<8; i++){
+      let row = [];
+      for(let j=0; j<8; j++){
+        row[j] = {
+          id: `${i+1}${String.fromCharCode(65+j)}`,
+          color: (i % 2 === 0 && j % 2 === 0) || (i % 2 !== 0 && j % 2 !== 0) ? 'black' : 'white',
+          piece: null,
+          activeState: false
+        }
+      }
+      board[i] = row;
+    }
+    this.state = {
+      previousMoves : [],
+      actualBoard : board
+    }
+  }
   render(){
     return(
       <div>
-        {/*Generate Board here*/}
+        {/*Generate Board here*/console.log(this.state.actualBoard)}
       </div>
     )
   }
