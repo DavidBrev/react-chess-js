5eb431292dc17becbf61650df6a3a02d37b09c94
DavidBrev <brevers.david@gmail.com>
2020-10-29 14:17:15 +0100

    Remove build folder from gitignore

---
+++ build/precache-manifest.84edadc4be828f4758a44238ed720d58.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c2fc9235f523bb33de0753357379b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba55bc1c706006e648f3",
+    "url": "./static/css/main.842af056.chunk.css"
+  },
+  {
+    "revision": "e4b1d5b8e1698845be42",
+    "url": "./static/js/2.cd3299ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cd3299ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba55bc1c706006e648f3",
+    "url": "./static/js/main.cb275e62.chunk.js"
+  },
+  {
+    "revision": "651cdcbd2b0d2f0322fe",
+    "url": "./static/js/runtime-main.cf39c841.js"
+  },
+  {
+    "revision": "a2f30e8ec78648d33ee846c786470c60",
+    "url": "./static/media/active.a2f30e8e.svg"
+  },
+  {
+    "revision": "1019b942dfc394f2a217852f6b49f812",
+    "url": "./static/media/bb.1019b942.svg"
+  },
+  {
+    "revision": "43a817d6f609ca511dbf11fb5babd677",
+    "url": "./static/media/bk.43a817d6.svg"
+  },
+  {
+    "revision": "f412a939a01592ed1dc71282b9211b39",
+    "url": "./static/media/bn.f412a939.svg"
+  },
+  {
+    "revision": "59b9362375b19c39a9e7604577623c22",
+    "url": "./static/media/bp.59b93623.svg"
+  },
+  {
+    "revision": "06fcbc607c7f7c090b3a21b9bf590feb",
+    "url": "./static/media/bq.06fcbc60.svg"
+  },
+  {
+    "revision": "c5de80bb54ffa16ef0b58284c1e6f124",
+    "url": "./static/media/br.c5de80bb.svg"
+  },
+  {
+    "revision": "5a1e4dd25653ace2594372867c73079b",
+    "url": "./static/media/wb.5a1e4dd2.svg"
+  },
+  {
+    "revision": "b25fe65a33f944a043f3b472153e3f9b",
+    "url": "./static/media/wk.b25fe65a.svg"
+  },
+  {
+    "revision": "efc8f3e732c8ec4d4fc96a21dfd6f73d",
+    "url": "./static/media/wn.efc8f3e7.svg"
+  },
+  {
+    "revision": "6e435dc32cba07970801543cd3fa0a7b",
+    "url": "./static/media/wp.6e435dc3.svg"
+  },
+  {
+    "revision": "45cf36b6fa4a81235ceb8ad8904ed99a",
+    "url": "./static/media/wq.45cf36b6.svg"
+  },
+  {
+    "revision": "8df1e1c2d33f9f1f31108df72668117f",
+    "url": "./static/media/wr.8df1e1c2.svg"
+  }
+]);
