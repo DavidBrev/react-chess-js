c398b081e7bc66ca0a522db26c4d26770e3a48ea
DavidBrev <brevers.david@gmail.com>
2020-10-31 23:18:05 +0100

    Modify promotion component to use vex library

--- src/Components/SelectReplacement.js
+++ src/Components/SelectReplacement.js
@@ -9,17 +9,22 @@ export default class SelectReplacement extends React.Component {
     if(this.props.selection){
       return (
         <div>
-          <p>Please select a promotion for your pawn</p>
-          <table>
-            <tbody>
-              <tr>
-                <td onClick={() => this.props.onPromoteClick(queen)} className={`tile ${queen}`}></td>
-                <td onClick={() => this.props.onPromoteClick(rook)} className={`tile ${rook}`}></td>
-                <td onClick={() => this.props.onPromoteClick(bishop)} className={`tile ${bishop}`}></td>
-                <td onClick={() => this.props.onPromoteClick(knight)} className={`tile ${knight}`}></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="vex-overlay"></div>
+          <div className="vex vex-theme-default">
+            <div className="vex-content">
+              <p>Please select a promotion for your pawn</p>
+              <table>
+                <tbody>
+                  <tr>
+                    <td onClick={() => this.props.onPromoteClick(queen)} className={`tile ${queen}`}></td>
+                    <td onClick={() => this.props.onPromoteClick(rook)} className={`tile ${rook}`}></td>
+                    <td onClick={() => this.props.onPromoteClick(bishop)} className={`tile ${bishop}`}></td>
+                    <td onClick={() => this.props.onPromoteClick(knight)} className={`tile ${knight}`}></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       );
     }
